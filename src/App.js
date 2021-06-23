7aa9a0e132539154d21236863fce0a4c016717a3
JeonMinJae <ssi02014@naver.com>
2021-06-23 22:22:57 +0900

    Add(minjae/modal): checkbox allcheck, unAllCheck 구현 #4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import SetRegionalComponent from "./components/SetRegionalComponent";
 
@@ -12,6 +13,22 @@ const Container = styled.div`
 `;
 
 function App() {
+  useEffect(() => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    axios
+      .get(
+        `https://motov-coding-homework.s3.ap-northeast-2.amazonaws.com/country.json`,
+        config
+      )
+      .then((res) => {
+        console.log(res);
+      });
+  }, []);
   return (
     <Container>
       <SetRegionalComponent />
--- src/components/ModalComponent.js
+++ src/components/ModalComponent.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
-
-const FormTitle = styled.h1`
-  margin-top: 0;
-`;
 
 const FormContainer = styled.form`
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 500px;
+  width: 600px;
   padding: 1rem;
   z-index: 10;
   background-color: #fff;
   border: 1px solid #999;
   border-radius: 8px;
+`;
+
+const FormTitle = styled.h1`
+  margin-top: 0;
+`;
+
+const FormButton = styled.button`
+  margin-right: 10px;
 `;
 
 const SelectBoxContainer = styled.select`
@@ -28,12 +32,30 @@ const SelectBoxContainer = styled.select`
 `;
 
 const CheckboxContainer = styled.div`
-  margin-top: 30px;
+  margin: 30px 0;
 `;
 
-const selectRegionContainer = styled.div``;
+const SelectRegionContainer = styled.div``;
 
 const ModalComponent = ({ modal }) => {
+  const checkBoxRef = useRef(null);
+
+  const allCheckHandle = (e) => {
+    e.preventDefault();
+
+    checkBoxRef.current.childNodes.forEach((el) => {
+      el.childNodes[0].checked = true;
+    });
+  };
+
+  const allUnCheckHandle = (e) => {
+    e.preventDefault();
+
+    checkBoxRef.current.childNodes.forEach((el) => {
+      el.childNodes[0].checked = false;
+    });
+  };
+
   return (
     <>
       {modal ? (
@@ -56,42 +78,44 @@ const ModalComponent = ({ modal }) => {
             <option value="서울특별시">강원도</option>
             <option value="서울특별시">강원도</option>
           </SelectBoxContainer>
-          <CheckboxContainer>
+          <FormButton onClick={allCheckHandle}>전체 선택</FormButton>
+          <FormButton onClick={allUnCheckHandle}>선택 해제</FormButton>
+          <CheckboxContainer ref={checkBoxRef}>
             <label>
-              <input type="checkbox" name="color" value="blue" />
+              <input type="checkbox" name="region" value="blue" />
               강남구
             </label>
             <label>
-              <input type="checkbox" name="color" value="red" />
+              <input type="checkbox" name="region" value="red" />
               강동구
             </label>
             <label>
-              <input type="checkbox" name="color" value="red" />
+              <input type="checkbox" name="region" value="red" />
               강동구
             </label>
             <label>
-              <input type="checkbox" name="color" value="red" />
+              <input type="checkbox" name="region" value="red" />
               강동구
             </label>
             <label>
-              <input type="checkbox" name="color" value="red" />
+              <input type="checkbox" name="region" value="red" />
               강동구
             </label>
             <label>
-              <input type="checkbox" name="color" value="red" />
+              <input type="checkbox" name="region" value="red" />
               강동구
             </label>
             <label>
-              <input type="checkbox" name="color" value="red" />
+              <input type="checkbox" name="region" value="red" />
               강동구
             </label>
           </CheckboxContainer>
-          <selectRegionContainer>
+          <SelectRegionContainer>
             <span>강남구</span>
             <span>마포구</span>
             <span>종로구</span>
             <span>서초구</span>
-          </selectRegionContainer>
+          </SelectRegionContainer>
         </FormContainer>
       ) : (
         <></>
